5632adfcf52a166341f52d537afb02459f66df63
Ian Mac Moore <ianmac@blueplusred.com>
2023-11-22 22:00:48 -0600

    Changed to vue components and first build

---
+++ src/components/landing.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <article class="mw7 center ph3 ph5-ns tc br2 pv5 mid-gray mb5">
+      <h1 class="fw6 f1 f2-ns lh-title mt0 mb3 monospace">
+        See Newly.
+      </h1>
+      <img class="w-50" src="/src/assets/img/bluepredvesica_v5.4_vert.png" alt="Blue Plus Red Logo">
+      <h2 class="fw2 f4 lh-copy mt0 mb3">
+        Helping inspire discovery.
+      </h2>
+      <p class="fw1 f5 mt0 mb3">
+        Bespoke visualizations. Software development and process consulting.
+      </p>
+      <div>
+        <a class="f6 br-pill bg-bpr-purple no-underline washed-green ba b--dark-gray pv2 ph3 dib mr3"
+          href="https://www.linkedin.com/in/imacmoore/">
+          Contact <i class="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
+    </article>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
+/* Set up using Vue 3 */
 import { createApp } from 'vue'
-import './style.css'
+import './css/tachyons.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Currently using fontawesome in CSS only
+// /* import the fontawesome core */
+// import { library } from '@fortawesome/fontawesome-svg-core'
+
+// /* import font awesome icon component */
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// /* import specific icons */
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+// /* add icons to the library */
+// library.add(faUserSecret)
+
+createApp(App)
+.mount('#app')
+
