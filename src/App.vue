aa891348efa0cf5278261ec97c67109e3a66f873
Ian Mac Moore <ianmac@blueplusred.com>
2023-12-05 22:50:12 -0600

    Changed filename to appease Vue build

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script setup>
-  import Landing from "./components/Landing.vue";
+  import Landing from "./components/Landing_.vue";
   import LineChart from "./components/LineChart.vue";
   import VizCards from "./components/VizCards.vue";
 </script>
