975d917c1b69b70e3a248f240fb61cc25f47178d
Ian Mac Moore <ianmac@blueplusred.com>
2023-12-12 18:10:42 -0600

    Added to Readme and shuffled nav

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-Homepage for Blue Plus Red LLC
-
+## Homepage for Blue Plus Red LLC
+	![Screenshot of landing page](./src/assets/img/Screen%20Shot%202023-12-12%20at%205.21.27%20PM.png)
 ## Attributions
+
 ### Built With
- - [tachyons css](https://tachyons.io/)
- - [vue js](https://vuejs.org/)
+
+- [tachyons css](https://tachyons.io/)
+- [vue js](https://vuejs.org/)
+- [vite](https://vitejs.dev/)
+- [supabase](https://supabase.com/)
+- [Vercel](https://vercel.com/)
+- [ApexCharts](https://github.com/apexcharts/vue-apexcharts)
+- [Google Fonts](https://fonts.google.com/)
+- [FontAwesome Brand Icons](https://fontawesome.com/icons)
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <Landing />
   <nav class="ph3 ph5-ns pb2 pt4">
-    <div class="flex nowrap overflow-x-auto bt bb bw1 b--mid-gray justify-center monospace">
-      <a class="link gray f5 f4-ns dib pa2  mh2" href="#" title=""
-      :class="{ active: activeView === 'VizCards' }"
-      @click="activeView = 'VizCards'"
-        >Cool Vizzes. (Not mine.)</a>
+    <div
+      class="flex nowrap overflow-x-auto bb bw1 b--mid-gray justify-center monospace">
       <a
         class="link gray f5 f4-ns dib mr4 pa2 mh2"
         href="#"
         title="Energy Use"
         :class="{ active: activeView === 'ApexLineChart' }"
-      @click="activeView = 'ApexLineChart'"
+        @click="activeView = 'ApexLineChart'"
         >Energy Use</a
+      >
+      <a
+        class="link gray f5 f4-ns dib pa2 mh2"
+        href="#"
+        title=""
+        :class="{ active: activeView === 'VizCards' }"
+        @click="activeView = 'VizCards'"
+        >Cool Vizzes. (Not mine.)</a
       >
     </div>
   </nav>
@@ -28,22 +33,20 @@ import VizCards from "./components/VizCards.vue";
 import BottomNav from "./components/BottomNav.vue";
 import ApexLineChart from "./components/ApexLineChart.vue";
 
-
 export default {
-  name: 'App',
+  name: "App",
   components: { Landing, VizCards, BottomNav, ApexLineChart },
   data() {
     return {
       activeView: "ApexLineChart",
-      dataset: null
-    }
+      dataset: null,
+    };
   },
   methods: {
     toggleModal() {
-      this.showModal = !this.showModal
+      this.showModal = !this.showModal;
     },
-    
-  }
+  },
 };
 </script>
 
