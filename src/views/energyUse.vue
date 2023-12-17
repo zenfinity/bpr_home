1f66237d08f726fe413ef5667310fdaf4806ff9a
Ian Mac Moore <ianmac@blueplusred.com>
2023-12-16 21:51:03 -0600

    Changed text to colored arrow icon

--- src/views/energyUse.vue
+++ src/views/energyUse.vue
@@ -1,17 +1,20 @@
-
 <template>
   <article class="pv3 ph5">
     <h2 class="gray tc underline">Exploring Work From Home Energy</h2>
     <ApexLineChart />
-    <h2 class="gray tc" v-text="'Reduction of total use over period: ' + reduction + '%'"></h2>
+
+    <div class="tc f3">
+      <i class="fa-solid fa-arrow-down green ph2"></i>
+      <span class="gray" v-text="reduction + '% over period'"></span>
+    </div>
+
     <ApexBarChart />
   </article>
-  
 </template>
 
 <script>
-import ApexLineChart from '../components/ApexLineChart.vue';
-import ApexBarChart from '../components/ApexBarChart.vue';
+import ApexLineChart from "../components/ApexLineChart.vue";
+import ApexBarChart from "../components/ApexBarChart.vue";
 
 import { supabase } from "../lib/supabaseClient";
 
@@ -22,14 +25,17 @@ const summaryTableResponse = await supabase.from("summaryUseXCel").select("*");
 // console.log(summaryTableResponse.data[0].sum)
 
 // percent_decrease = ((kWh_2021 - kWh_2023) / kWh_2021) * 100
-const percentReduced = ((summaryTableResponse.data[0].sum - summaryTableResponse.data[2].sum) / summaryTableResponse.data[0].sum) * 100
+const percentReduced =
+  ((summaryTableResponse.data[0].sum - summaryTableResponse.data[2].sum) /
+    summaryTableResponse.data[0].sum) *
+  100;
 // console.log(percentReduced.toFixed(2))
 export default {
   name: "EnergyUse",
   components: { ApexLineChart, ApexBarChart },
   data() {
     return {
-      reduction: percentReduced.toFixed(0)
+      reduction: percentReduced.toFixed(0),
     };
   },
   methods: {},
